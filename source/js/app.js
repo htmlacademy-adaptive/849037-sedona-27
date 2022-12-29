9507c28ac0ce983d9b545cb88a8d72d5611513ef
Max <props98@yahoo.com>
2022-12-29 16:33:24 +0300

    Добавленны иконки для меню мобильной версии, сделан интерактив

--- source/js/app.js
+++ source/js/app.js
@@ -2,7 +2,9 @@
 
 const btnMenu = document.querySelector('.main-header__toggle'),
       header = document.querySelector('.main-header'),
-      menu = document.querySelector('.main-header__nav');
+      menu = document.querySelector('.main-header__nav'),
+      iconOpen = document.querySelector('.main-header__icon-btn--open'),
+      iconClose = document.querySelector('.main-header__icon-btn--close');
 
 header.classList.remove('main-header--nojs');
 
@@ -14,4 +16,13 @@ btnMenu.addEventListener('click', (e) => {
     menu.classList.add('main-header__nav--closed');
     menu.classList.remove('main-header__nav--opened');
   }
+
+  if (menu.classList.contains('main-header__nav--closed')) {
+    iconOpen.classList.remove('visually-hidden');
+    iconClose.classList.add('visually-hidden');
+  } else {
+    iconOpen.classList.add('visually-hidden');
+    iconClose.classList.remove('visually-hidden');
+  }
+
 });
